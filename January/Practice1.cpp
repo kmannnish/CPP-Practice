f624ee98903cdabf6682d0ca0da9c0c71ca4db05
Manish Kumawat <62220845+kmannnish@users.noreply.github.com>
2023-04-01 10:43:09 +0530

    time complexity AIETM Practice1.cpp

--- January/Practice1.cpp
+++ January/Practice1.cpp
@@ -49,3 +49,70 @@ int main(){
 		cout<<"NO! It is not Palindrome"<<endl;
 	}
 }
+
+//Time Complexity Calculation
+
+
+#include <iostream> 
+ #include <algorithm> 
+ using namespace std; 
+  
+ void intersection(int *arr1, int *arr2, int n, int m) 
+ { 
+     sort(arr1, arr1 + n); //
+     sort(arr2, arr2 + m); 
+     int i = 0, j = 0; 
+     while (i < n && j < m) //nm
+     { 
+         if (arr1[i] == arr2[j]) //n
+         { 
+             cout << arr1[i] << " "; 
+             i++; 
+             j++; 
+         } 
+         else if (arr1[i] < arr2[j]) //n
+         { 
+             i++; 
+         } 
+         else //n^2
+         { 
+             j++; 
+         } 
+     } 
+ } 
+  
+ int main() 
+ { 
+     int t; 
+     cin >> t; 
+     while (t--) // from t, t-1 ......1, 0
+     { 
+  
+         int size1, size2; 
+  
+         cin >> size1; 
+         int *input1 = new int[size1]; 
+  
+         for (int i = 0; i < size1; i++) // n = input from user
+         { 
+             cin >> input1[i]; 
+         } 
+  
+         cin >> size2; 
+         int *input2 = new int[size2]; 
+  
+         for (int i = 0; i < size2; i++) // n
+         { 
+             cin >> input2[i]; 
+         } 
+  
+         intersection(input1, input2, size1, size2); 
+  
+         delete[] input1; 
+         delete[] input2; 
+  
+         cout << endl; 
+     } 
+  
+     return 0; 
+ }
